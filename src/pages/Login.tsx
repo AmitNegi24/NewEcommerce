775932d81d9b3108ad3b79e4a6108e9031036ee7
AmitNegi24 <mramitnegig1999@gmail.com>
2024-01-08 13:31:54 +0530

    commit

---
+++ src/pages/Login.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react"
+
+function Login() {
+    const [gender,setGender]=useState("");
+    const [date,setDate]=useState("");
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
